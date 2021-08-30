1d504ae1092dd4051b2f8d879458a26a53ef9abc
Hung-I Wang <whygowe@gmail.com>
2021-08-30 17:45:57 +0800

    No referrer for 3rd-party resources

--- api/readability.js
+++ api/readability.js
@@ -69,6 +69,7 @@ function render(meta) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="referrer" content="same-origin">
   <meta http-equiv="Content-Security-Policy" content="script-src 'none';">
   <meta http-equiv="Content-Security-Policy" content="frame-src 'none';">
   <meta name="description" content="${excerpt}">
